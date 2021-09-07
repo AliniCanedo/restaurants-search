4044369f7e74d22adfc2ef9a468348e8ffbea4f9
Alini Canedo <alini.canedo@gmail.com>
2021-09-07 18:49:28 -0300

    reset styled

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import { ThemeProvider } from 'styled-components';
+import { Reset } from 'styled-reset';
 
 import theme from './theme';
 import Home from './pages/home';
@@ -8,6 +9,7 @@ import Home from './pages/home';
 function App() {
   return (
     <ThemeProvider theme={theme}>
+      <Reset />
       <Home />
     </ThemeProvider>
   );
